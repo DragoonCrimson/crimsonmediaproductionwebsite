b9f888b2ac94123e5efe807c772a826a0bc6e839
DragoonCrimson <110416411+DragoonCrimson@users.noreply.github.com>
2023-02-16 21:53:13 +0100

    Update financeCalculator.js

--- financeCalculator.js
+++ financeCalculator.js
@@ -11,6 +11,7 @@ function calculations() {
   console.log(percentagePaid.toFixed(2) + "%");
   response.innerText = "You have paid: " + (percentagePaid.toFixed(2) + "%") + " tax on your income";
   secondResponse.innerText = "This is the monetary equivalent to: " + difference;
+  event.preventDefault();
 }
 const grossIncomeForm = document.getElementById("grossIncomeForm");
 
